74e2ffcce36b303d58bc134a168c623d51ea86a2
Michael Hunt <mike038@btinternet.com>
2016-01-13 16:55:54 +0000

    wind db started

---
+++ cornwallWind/windProjects.py
@@ -0,0 +1,61 @@
+import urllib
+import sqlite3
+import json
+import time
+import ssl
+
+serviceurl = "http://maps.googleapis.com/maps/api/geocode/json?"
+
+# Deal with SSL certificate anomalies Python > 2.7
+# scontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+scontext = None
+
+conn = sqlite3.connect('windProjects.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Projects (
+    project TEXT,
+    region TEXT,
+    location TEXT,
+    turbines INT,
+    project_capacity FLOAT,
+    turbine_capacity FLOAT,
+    developer TEXT,
+    current_status_date TEXT,
+    status TEXT,
+    onoff TEXT  
+    )''')
+
+fh = open("turbines.txt","r")
+print "Hello"
+count = 0
+for line in fh:
+    #print "Hello",line
+    count += 1
+nProjects=count/10
+print "nProjects: ",nProjects
+fh.close()
+fh = open("turbines.txt","r")
+
+for i in range (nProjects):
+    print i
+    project =fh.readline().strip()
+    print project
+    region =fh.readline().strip()
+    print region
+    location =fh.readline().strip()
+    print location
+    turbines =int(fh.readline().strip())
+    print turbines
+    project_capacity =float(fh.readline().strip())
+    turbine_capacity =float(fh.readline().strip())
+    developer =fh.readline().strip()
+    current_status_date =fh.readline().strip()
+    status =fh.readline().strip()
+    onoff =fh.readline().strip()
+        
+    cur.execute('''INSERT INTO Projects (project,region,location,turbines,project_capacity,turbine_capacity,developer,current_status_date,status,onoff)
+              VALUES ( ?,?,?,?,?,?,?,?,?,? ) ''',(project,region,location,turbines,project_capacity,turbine_capacity,developer,current_status_date,status,onoff))
+    conn.commit() 
+fh.close()
