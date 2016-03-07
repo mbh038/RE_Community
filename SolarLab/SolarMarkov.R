f8e0609e6c685d796ba4bedd74cb7cc1ccafb0b4
Michael Hunt <mike038@btinternet.com>
2016-03-07 16:42:28 +0000

    Cam2001 added

---
+++ SolarLab/SolarMarkov.R
@@ -0,0 +1,53 @@
+##
+
+
+deltaOdot<-function(t){
+  # t is the time in day number 1-365
+  #t<-t*365
+  asin(sin(-0.4091)*cos((2*pi/365.24)*(t+10)+0.0334*sin((2*pi/365.24)*(t-2))))
+}
+
+h0<-function(phi,t){
+  acos(tan(phi)*tan(deltaOdot(t)))
+}
+
+cosTheta<-function(phi,delta,h){
+  # theta is the zenith angle
+  sin(phi)*sin(delta)+cos(phi)*cos(delta)*cos(h)
+}
+
+solarFlux<-function(S0,phi,t){
+  h<-pi*(2*(t-floor(t))-1)
+  delta<-deltaOdot(t)
+  flux<-S0*cosTheta(phi,delta,h)
+  flux<-pmax(flux,rep(0,length(t)))
+  flux
+}
+
+#####################################################################
+
+library(lubridate)
+Cam2001<- read.table("./data/project_label_BSRN_event_label_CAM_2001.tab",sep="\t", header=TRUE,stringsAsFactors=FALSE)
+str(Cam2001)
+names(Cam2001)<-c("datetime","DIF","LWD","SWD","DIR","ORG")
+
+# convert time to POSIXct
+Cam2001$Date.Time<-ymd_hms(Cam2001$datetime)
+
+attach(Cam2001)
+
+Npoint<-7*1440
+plot(SWD[1:Npoint],type="l")
+
+
+######################################################################
+day<-307
+phi=(pi/180)*50
+S0=1368
+t=seq(1,366,length.out=365*1440)
+daybegin<-day*1440
+dayend<-(day+7)*1440
+Q<-solarFlux(S0,phi,t)
+
+plot(SWD[daybegin:dayend],type="l",ylim=c(0,1500))
+lines(Q[daybegin:dayend],type="l",col="blue")
